1e5641e806bde0af688cfdca708254399b40724b
elias.vulcano.lubricacion@gmail.com <eliasrobledoxx@hotmail.com>
2026-01-14 01:50:45 +0100

    feat(Contact entity): Change the property names from OwnerUserId and ContactUserId to AddresseeId and RequesterId

---
+++ Infrastructure/Migrations/20260113160844_ChangePropertyNamesOfContactEntity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePropertyNamesOfContactEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ContactUserId",
+                table: "Contacts",
+                newName: "AddresseeId");
+
+            migrationBuilder.RenameColumn(
+                name: "OwnerUserId",
+                table: "Contacts",
+                newName: "RequesterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AddresseeId",
+                table: "Contacts",
+                newName: "ContactUserId");
+
+            migrationBuilder.RenameColumn(
+                name: "RequesterId",
+                table: "Contacts",
+                newName: "OwnerUserId");
+        }
+    }
+}
