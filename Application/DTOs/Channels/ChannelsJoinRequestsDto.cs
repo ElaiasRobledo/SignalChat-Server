ca699c52b32699fb6428985b5c5e90cbda1d3e8f
elias.vulcano.lubricacion@gmail.com <eliasrobledoxx@hotmail.com>
2026-02-25 21:03:45 +0100

    feat: add dto for creating join requests

---
+++ Application/DTOs/Channels/ChannelsJoinRequestsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Channels
+{
+    public record IncomingRequestsDto
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Reason { get; set; }
+        public DateTime Date { get; set; }
+    }
+    public record CreateJoinRequestDto
+    {
+        public string Reason { get; set; }
+
+    }
+}
