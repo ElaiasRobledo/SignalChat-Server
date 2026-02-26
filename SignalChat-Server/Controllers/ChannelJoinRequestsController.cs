3f354d917132cce18bb8eaef6d7dc897752bdb01
elias.vulcano.lubricacion@gmail.com <eliasrobledoxx@hotmail.com>
2026-02-26 01:33:14 +0100

    feat: add channel join requests controller for handling just the channel requests

---
+++ SignalChat-Server/Controllers/ChannelJoinRequestsController.cs
@@ -0,0 +1,86 @@
+﻿using Application.Common.Interfaces.Channels;
+using Application.Common.Interfaces.Utils;
+using Application.DTOs.Channels;
+using Application.Exceptions.ChannelMembers;
+using Application.Exceptions.Channels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalChat_Server.Hubs;
+
+namespace SignalChat_Server.Controllers
+{
+    [ApiController]
+    [Route("api/channels/{channelId}/requests")]
+    public class ChannelJoinRequestsController : ControllerBase
+    {
+        private readonly IChannelJoinRequest _channelJoinRequests;
+        private readonly IChannelMembership _channelMembership;
+        private readonly IChannelModeration _channelModeration;
+        private readonly IHubContext<ChatHub> _hub;
+        private readonly ICurrentUserService _currentUser;
+
+        private readonly ILogger<ChannelJoinRequestsController> _logger;
+
+        public ChannelJoinRequestsController(IChannelJoinRequest channelJoinRequests,
+            IChannelMembership channelMembership,
+            IChannelModeration channelModeration,
+            ILogger<ChannelJoinRequestsController> logger,
+            ICurrentUserService currentUserService,
+            IHubContext<ChatHub> hub)
+        {
+            _channelJoinRequests = channelJoinRequests;
+            _channelMembership = channelMembership;
+            _channelModeration = channelModeration;
+            _hub = hub;
+            _logger = logger;
+            _currentUser = currentUserService;
+        }
+
+        [HttpGet("pending")]
+        public async Task<IActionResult> GetIncomingRequests([FromRoute] Guid channelId)
+        {
+            try
+            {
+                var result = await _channelJoinRequests.IncomingRequestsAsync(channelId, _currentUser.UserId);
+                return Ok(result);
+            }
+            catch (MemberNotAuthorizedException ex)
+            { return Unauthorized(ex.Message); }
+            catch (ChannelNotFoundException ex)
+            { return NotFound(ex.Message); }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [HttpPost("join")]
+        public async Task<IActionResult> SendJoinRequest(CreateJoinRequestDto request, [FromRoute] Guid channelId)
+        {
+            try
+            {
+                await _channelJoinRequests.CreateJoinRequestAsync(
+               _currentUser.UserId, channelId, request.Reason);
+            }
+            catch (ChannelNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (SentRequestToJoinToChannelException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch(OwnerSendJoinRequestException ex)
+            {
+                return BadRequest(ex.Message); 
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Created();
+        }
+
+    }
+}
